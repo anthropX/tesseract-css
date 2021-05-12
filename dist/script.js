c173030d5c8a5d1067cef6b3aa14f08dfa2c7255
Vimal Patel <vimal@quizizz.com>
2021-05-12 11:19:27 +0530

    feat: create a tesseract

---
+++ dist/script.js
@@ -0,0 +1,19 @@
+const player = Vue.createApp({
+  data() {
+    return {
+      
+    }
+  },
+  created() {
+  },
+  destroyed() {
+  },
+  methods: {
+    m1() {
+
+    }
+  }
+    
+});
+
+player.mount('.scene');
